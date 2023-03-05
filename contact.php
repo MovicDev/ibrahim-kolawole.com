4455fc54a1c5c727408db457f52aa601229b6a96
ibrahim kolawole <86012048+moviv@users.noreply.github.com>
2023-03-05 06:28:43 -0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/responsible.css" rel="stylesheet" type="text/css">
+	
+
+<body>
+	<header>
+						 <img src="images/logo.png"/>	
+			<nav>
+				<ul>
+										
+				<li><a href="index.html" class="is-active">Home</a></li>
+				<li><a href="contact.php">Contact</a></li>
+				</ul>
+			
+			</nav>
+		
+		</header>	<br>
+	<br>	<br>	<br>	<br>
+	<h1>Contact</h1>  
+	    	
+	<section>
+		
+		<contact>
+		Professional Web Programmer from Nigeria Ibadan<br><br>
+Website:
+www.ibrahimkolawole.com<br><br>
+City:
+Ibadan, Nigeria<br><br>
+Ph:
++234 901 6504 151<br><br>
+Em:
+ibrahimkolawole564@gmail.com
+		<contact>
+		
+			<contact>
+		<form>
+			Your name
+			<input type="text" name="" placeholder="your name"><br><br>
+			
+			Subject
+			<input type="text" name="" placeholder="subject"><br><br>
+			
+			Message us
+			<textarea name="" cols="70px"; rows="10px;" placeholder="Message us"></textarea>
+			
+			
+			
+			
+			</form>
+		
+		
+	
+		</contact>
+		</section>
+</body>
+</html>
